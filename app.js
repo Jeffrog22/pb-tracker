99034ee40b350b3aa3e0e1f9d3bcb3bef336002a
PBTracker <jeffux@hotmail.com>
2026-08-26 00:31:41 -0300

    chore: APP_VERSION 0.20.12

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import {
 } from "./utils.js";
 import { initSwimBase, renderSwimBaseScreen } from "./swimbase.js";
 
-const APP_VERSION = "0.20.11";
+const APP_VERSION = "0.20.12";
 
 const state = {
   teamName: "",
