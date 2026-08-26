98f6030a3ea1ca0deb58426cbf8a44af86739387
PBTracker <jeffux@hotmail.com>
2026-08-26 00:30:53 -0300

    chore: bump APP_VERSION para 0.20.11

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import {
 } from "./utils.js";
 import { initSwimBase, renderSwimBaseScreen } from "./swimbase.js";
 
-const APP_VERSION = "0.20.9";
+const APP_VERSION = "0.20.11";
 
 const state = {
   teamName: "",
